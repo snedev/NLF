4a78d620f58e7681a83de7c25d6d4218a0336f8a
Stan Nedev <stan@nedev.io>
2019-08-15 13:08:58 +0300

    Changed the way a new import file is created

--- dashboard/create_import_file.py
+++ dashboard/create_import_file.py
@@ -10,23 +10,21 @@ script, filename = argv
 
 with open(filename, 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
+    lines = []
+    for line in csv_reader:
+        d = line['Date']
+        date_formatted = time.mktime(datetime.datetime.strptime(d, "%Y-%m-%d %I-%p").timetuple())
+        line['Date'] = int(date_formatted)
+        lines.append(line)
+with open(filename, 'w') as new_file:
+    fieldnames = ['Date', 'Symbol', 'Price']
+    csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
+    csv_writer.writeheader()  # Write the headers for the columns in the new .csv file
+    csv_writer.writerows(lines)
 
-    with open('formatted.csv', 'w') as new_file:
-        fieldnames = ['Date', 'Symbol', 'Price']
+# Take the new_file stored in memory and create a line protocol .txt file for importing into influxDB
 
-        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
-        csv_writer.writeheader()  # Write the headers for the columns in the new .csv file
-
-        for line in csv_reader:
-            d = line['Date']
-            date_formatted = time.mktime(datetime.datetime.strptime(d, "%Y-%m-%d %I-%p").timetuple())
-            line['Date'] = int(date_formatted)
-            print(line['Date'])
-            csv_writer.writerow(line)
-
-# Take the new csv file and create a line protocol .txt file for importing into influxDB
-
-file = pd.read_csv("formatted.csv")
+file = pd.read_csv(filename)
 lines = [str(file["Symbol"][d])
          + " " + "Price=" + str(file["Price"][d])
          + " " + str(file["Date"][d]) for d in range(len(file))]
@@ -40,6 +38,3 @@ CREATE DATABASE NLF
 """)
 for item in lines:
     output.write("%s\n" % item)
-
-
-os.remove("formatted.csv")  # Delete the temp .csv file
