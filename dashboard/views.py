913a5af6add89845e48636aabaef236ad6f47517
Stan Nedev <stan@nedev.io>
2019-08-30 17:13:17 +0300

    Added data analysis instruments to homepage

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,10 @@
-import json, time, datetime
+import time, datetime
 import pandas as pd
+import numpy as np
 
 from django.shortcuts import redirect, render
 from influxdb import InfluxDBClient
 from django.http import JsonResponse
-from functools import reduce
 
 from NLF import settings
 
@@ -42,6 +42,7 @@ def get_data(request):
         date_from = str(int(time.mktime(datetime.datetime.strptime(date_from_unformatted, "%Y-%m-%dT%H:%M").timetuple()))) + "000000000"
         date_to_unformatted = request.POST.get('date_to')
         date_to = str(int(time.mktime(datetime.datetime.strptime(date_to_unformatted, "%Y-%m-%dT%H:%M").timetuple()))) + "000000000"
+        data_analysis = request.POST.get('data_analysis')
         dataframes = []
         for currency in currency_pairs:
             q = ('select Price from {} where time > {} AND time <= {}').format(currency, date_from, date_to)
@@ -55,6 +56,22 @@ def get_data(request):
         instruments = {'time': df_joined.index.tolist()}
         for i in currency_pairs:
             instruments[i] = df_joined[i].tolist()
-        return JsonResponse(instruments, safe=False)
+        if data_analysis == 'ma21':
+            df_joined['MA21'] = df_joined.rolling(min_periods=1, window=21).mean()
+            instruments.update({'MA21': df_joined['MA21'].tolist()})
+            return JsonResponse(instruments, safe=False)
+        elif data_analysis == 'ma100':
+            df_joined['MA100'] = df_joined.rolling(min_periods=1, window=100).mean()
+            instruments.update({'MA100': df_joined['MA100'].tolist()})
+            return JsonResponse(instruments, safe=False)
+        elif data_analysis == 'macd':
+            for i in currency_pairs:
+                df_joined['26EMA'] = df_joined[i].rolling(min_periods=1, window=26).mean()
+                df_joined['12EMA'] = df_joined[i].rolling(min_periods=1, window=12).mean()
+                df_joined['MACD'] = df_joined['12EMA'] - df_joined['26EMA']
+                instruments.update({'MACD': df_joined['MACD'].tolist()})
+                return JsonResponse(instruments, safe=False)
+        else:
+            return JsonResponse(instruments, safe=False)
     else:
         return redirect('home')
