da1d81fab66a8951932f553844b4f0345b9333a6
Stan Nedev <stan@nedev.io>
2019-09-18 22:42:23 +0300

    Added chart indicators support to Snips NLU

--- dashboard/views.py
+++ dashboard/views.py
@@ -45,7 +45,7 @@ def get_data(request):
         data_analysis = request.POST.get('data_analysis')
         dataframes = []
         for currency in currency_pairs:
-            q = ('select Price from {} where time > {} AND time <= {}').format(currency, date_from, date_to)
+            q = ('SELECT Price FROM {} WHERE TIME > {} AND TIME <= {}').format(currency, date_from, date_to)
             query = influxdb_client().query(q, chunked=True, chunk_size=1000)
             query = list(query.get_points(measurement=currency))
             df = pd.DataFrame(query).set_index('time')
@@ -87,6 +87,7 @@ def get_nlu(request):
         date_from = str(int(time.mktime(datetime.datetime.strptime(date_from_unformatted[:16], "%Y-%m-%d %H:%M").timetuple()))) + "000000000"
         date_to_unformatted = parsing['slots'][2]['value']['value']
         date_to = str(int(time.mktime(datetime.datetime.strptime(date_to_unformatted[:16], "%Y-%m-%d %H:%M").timetuple()))) + "000000000"
+        data_analysis = parsing['slots'][3]['value']['value']
         dataframes = []
         for currency in currency_pairs:
             q = ('select Price from {} where time > {} AND time <= {}').format(currency, date_from, date_to)
@@ -100,6 +101,22 @@ def get_nlu(request):
         instruments = {'time': df_joined.index.tolist()}
         for i in currency_pairs:
             instruments[i] = df_joined[i].tolist()
+        if data_analysis == 'MA21':
+            df_joined['MA21'] = df_joined.rolling(min_periods=1, window=21).mean()
+            instruments.update({'MA21': df_joined['MA21'].tolist()})
+            return JsonResponse(instruments, safe=False)
+        elif data_analysis == 'MA100':
+            df_joined['MA100'] = df_joined.rolling(min_periods=1, window=100).mean()
+            instruments.update({'MA100': df_joined['MA100'].tolist()})
+            return JsonResponse(instruments, safe=False)
+        elif data_analysis == 'MACD':
+            for i in currency_pairs:
+                df_joined['26EMA'] = df_joined[i].rolling(min_periods=1, window=26).mean()
+                df_joined['12EMA'] = df_joined[i].rolling(min_periods=1, window=12).mean()
+                df_joined['MACD'] = df_joined['12EMA'] - df_joined['26EMA']
+                instruments.update({'MACD': df_joined['MACD'].tolist()})
+                return JsonResponse(instruments, safe=False)
+        else:
             return JsonResponse(instruments, safe=False)
     else:
         return redirect('home')
